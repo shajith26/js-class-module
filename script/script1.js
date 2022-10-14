104a6247cbf3fe83c7d6eb4ac875c4bdb3da1496
shajith26 <mohamedshajith321@gmail.com>
2022-10-14 10:01:07 +0530

    class-module

---
+++ script/script1.js
@@ -0,0 +1,14 @@
+// class Car{
+//     name;
+//     year;
+//     constructor(Name,yr){
+//         this.name=Name;
+//         this.year=yr;
+//     }
+// }
+// const obj=new Car("BMW",2022);
+// console.log(obj)
+
+
+import {a} from "./script2.js";
+console.log(a);
